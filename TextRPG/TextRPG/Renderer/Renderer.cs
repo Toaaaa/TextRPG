8697d71d4ef1b31192cdf09fcdb6d44d0b6d355a
Junebeom Lee <junebeom.dev@gmail.com>
2025-02-11 15:22:03 +0900

    feat: 에러 시 메세지 커스터마이즈되도록 변경

--- TextRPG/TextRPG/Renderer/Renderer.cs
+++ TextRPG/TextRPG/Renderer/Renderer.cs
@@ -16,9 +16,9 @@ public class Renderer
     public Action<Renderer, States>? Register;
     
     // error handler
-    private bool _isInvalid = false;
-    public void Error() => _isInvalid = true;
-    private void _showError() { Logger.WriteLine("\n잘못된 선택입니다.", ConsoleColor.Red); _isInvalid = false; }
+    private string? _invalidateMessage = null;
+    public void Error(string? message = "잘못된 선택입니다.") => _invalidateMessage = message;
+    private void _showError() { Logger.WriteLine(_invalidateMessage, ConsoleColor.Red); _invalidateMessage = null; }
 
     public Renderer(Action<Renderer, States> register)
     {
@@ -40,7 +40,7 @@ public class Renderer
     {
         Console.Clear();
         this.Content();
-        if (_isInvalid) { _showError(); }
+        if (_invalidateMessage != null) { _showError(); }
         // fix: 선택 문구도 달라질 수 있음.
         // Console.WriteLine("\n원하시는 행동을 입력해주세요.\n>>");
         string? input = Console.ReadLine();
